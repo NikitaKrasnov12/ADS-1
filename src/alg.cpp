5b6806dcafcd21dc8eddffff0c8bae4a39bf7cc9
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-02-26 20:59:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,8 +19,8 @@ uint64_t nPrime(uint64_t n) {
     for (int i = 2; i < 1000; i++) {
         if (checkPrime(i)) {
             count += 1;
-            if (n == i) {
-                return count;
+            if (count == n) {
+                return i;
             }
         }
     }
