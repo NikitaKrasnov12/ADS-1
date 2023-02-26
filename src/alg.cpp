1240b697b9413dcb470f2fd1c159cf968b124715
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-02-26 20:52:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
     bool check = true;
-    for (uint64_t i = 2; i <= (value / 2); i++) {
+    for (int i = 2; i <= (value / 2); i++) {
         if (value % i == 0) {
             check = false;
             break;
@@ -15,8 +15,8 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t count = 0;
-    for (uint64_t i = 2; i < 1000; i++) {
+    int count = 0;
+    for (int i = 2; i < 1000; i++) {
         if (checkPrime(i)) {
             count += 1;
             if (n == i) {
@@ -27,7 +27,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-    for (uint64_t i = value + 1; i <= 1000; i++) {
+    for (int i = value + 1; i <= 1000; i++) {
         if (checkPrime(i)) {
             return i;
         }
@@ -35,8 +35,8 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t count = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
+    int count = 0;
+    for (int i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
             count += i;
         }
